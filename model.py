915e5b1ceec0118a82ffb57847e9f375851a2c3b
Tilen Klinc <tilen.cinc@gmail.com>
2020-05-11 19:12:16 +0200

    Dodal metodo za ugibanje

--- model.py
+++ model.py
@@ -58,6 +58,39 @@ class Igra:
                  return False
         return True
     
+    # izpiše nepravilna ugibanja ločena s presledkom
+    def nepravilni_ugibi(self):
+        return " ".join(self.napacne_crke())
 
-    
+    # vrne pravilni del gesla, ki izpiše le uganjene črke, preostale nadomesti z "_"
+    def pravilni_del_gesla(self):
+        trenutno = ""
+        for crka in self.geslo:
+            if crka in self.crke:
+                trenutno += crka
+            else:
+                trenutno += "_"
 
+        return trenutno
+
+    # metoda za ugibanje
+    def ugibaj(self, ugibana_crka):
+        ugibana_crka = ugibana_crka.lower()
+
+        if ugibana_crka in self.crke:
+            return PONOVLJENA_CRKA
+        
+        self.crke.append(ugibana_crka)
+
+        if ugibana_crka in self.geslo:
+            # je uganil:
+            if self.zmaga():
+                return ZMAGA
+            else:
+                return PRAVILNA_CRKA     
+
+        else:
+            if self.poraz():
+                return PORAZ
+            else:
+                return NAPACNA_CRKA
