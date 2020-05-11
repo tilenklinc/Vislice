b03c988803910f422646febbdb119b5b840c0267
Tilen Klinc <tilen.cinc@gmail.com>
2020-05-11 18:20:25 +0200

    Dodal bazen besed in metodo za štetje napak

--- model.py
+++ model.py
@@ -1,26 +1,40 @@
-# Konstante:
+# Konstante za rezultate ugibanj:
+STEVILO_DOVOLJENIH_NAPAK = 10
+PRAVILNA_CRKA = "+"
+PONOVLJENA_CRKA = "o"
+NAPACNA_CRKA = "-"
+# Konstante za zmago in poraz
+ZMAGA = "W"
+PORAZ = "X"
 
-stevilo_dovoljenih_napak = 10
-pravilna_crka = "+"
-ponovljena_crka = "○"
-napacna_crka = "-"
-zmaga = "W"
-poraz = "L"
+# Odpremo datoteko in ločimo besede
+bazen_besed =  []
+with open("Git\Vislice\\besede.txt", encoding="utf-8") as datoteka_bazena:
+    for beseda in datoteka_bazena:
+        bazen_besed.append(beseda.strip().lower())
 
 #definiram razred
 class Igra:
     #nastavim vrednost spremenljivk
-    def __init__(self, geslo, crke):
+    def __init__(self, geslo, crke=None):
         self.geslo = geslo
-        self.crke = poskusi_igralca
+        if crke is None:
+            self.crke = []
+        else:
+            self.crke = crke
 
 
-    #definiram metodi
-    def pravilne_crke(poskusi_igralca):
+    # Metoda za ustvarjanje seznamov pravilnih in napačnih črk
+    def pravilne_crke(crke):
         seznam_pravilnih = []
         seznam_napacnih = []
-        for znak in poskusi_igralca:
+        for znak in crke:
             if znak in self.geslo:
                 seznam_pravilnih.append(znak)
             else:
                 seznam_napacnih.append(znak)
+
+    # Preštejem število napačnih ugibanj
+    def stevilo_napak():
+        len(seznam_napacnih)
+
