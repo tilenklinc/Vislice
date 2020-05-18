9a06cc96177965fbb24e73f52660106d3d5e80a4
Tilen Klinc <tilen.cinc@gmail.com>
2020-05-18 18:26:41 +0200

    Dodal datoteke in popravil datoteko vislice

--- vislice.py
+++ vislice.py
@@ -1,3 +1,13 @@
 import bottle
 
 import model
+
+vislice = model.Vislice()
+
+@bottle.get("/")
+def index():
+    return bottle.template("Dokumenti\\2019_20\\UVP\\Git\\Visliceindex.tpl")
+
+
+
+bottle.run(reloader=True, debug=True)
